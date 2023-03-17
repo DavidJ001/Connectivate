a68da9c05b1031c49935a815de4f668ea75af591
DavidJ001 <Davidjoel392@gmail.com>
2023-03-17 22:26:45 +0300

    password checker

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -38,7 +38,7 @@ export const register = async (req, res) => {
   }
 };
 
-/* LOGGING IN */
+/* LOGGING IN Password check */
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;

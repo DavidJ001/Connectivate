d2b83862d63958157a87994307f8df412d9e7955
DavidJ001 <Davidjoel392@gmail.com>
2023-03-22 23:06:38 +0300

    major updates

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -38,7 +38,7 @@ export const register = async (req, res) => {
   }
 };
 
-/* LOGGING IN */
+/* LOGGING IN (password check) */
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
